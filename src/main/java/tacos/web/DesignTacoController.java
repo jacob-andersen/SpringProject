ec91293909e7014b780365c0e5a6290ed5a7cd5d
Jacob Andersen <Jacobma87@gmail.com>
2019-02-03 21:17:54 -0500

    working on order controller

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -21,13 +21,12 @@ import tacos.Ingredient.Type;
 @Slf4j
 @Controller
 @RequestMapping("/design")
-
-
-    public class DesignTacoController {
+public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList( new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
                 new Ingredient("CARN", "Carnitas", Type.PROTEIN),
@@ -36,7 +35,8 @@ import tacos.Ingredient.Type;
                 new Ingredient("CHED", "Cheddar", Type.CHEESE),
                 new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE) );
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
 
             Type[] types = Ingredient.Type.values();
             for (Type type : types) {
@@ -48,6 +48,13 @@ import tacos.Ingredient.Type;
 
             return "design";
     }
+
+    @PostMapping
+    public String processDesign(Design design) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
     private List<Ingredient> filterByType(
     <Ingredient> ingredients, Type type) {
         return ingredients
